a79bb8463a37c415ce07bb2e87e1b43dd39d1699
SkySarDev <skysar@ya.ru>
2021-11-03 19:46:23 +0300

    feat: add Publishers page

---
+++ src/containers/AllPublishersContainer.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchContent } from "store/reducers/mainContentReducer/actions";
+import { ROUTE_PUBLISHERS } from "utils/constants";
+
+import ContentHeader from "components/UI/ContentHeader";
+import CardsListPage from "components/pages/CardsListPage";
+
+const AllPublishersContainer = () => {
+  const { data, bgImage, loading } = useSelector((state) => state.mainContent);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContent(ROUTE_PUBLISHERS));
+  }, [dispatch]);
+
+  return (
+    <>
+      {loading || !data.publishers ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <ContentHeader image={bgImage} />
+          <CardsListPage
+            title={"Publishers"}
+            cardsList={data.publishers}
+            route={ROUTE_PUBLISHERS}
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AllPublishersContainer;
