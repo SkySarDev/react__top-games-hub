87c24ff71ec1367d67f7676e927d66d3a6964853
SkySarDev <skysar@ya.ru>
2021-12-11 20:31:23 +0300

    feat: add infinite scroll for Games

--- src/containers/AllGamesContainer.jsx
+++ src/containers/AllGamesContainer.jsx
@@ -2,13 +2,18 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 
-import { fetchContent } from "store/reducers/mainContentReducer/actions";
+import {
+  fetchContent,
+  fetchMoreData,
+} from "store/reducers/mainContentReducer/actions";
 import { ROUTE_GAMES } from "utils/constants";
 
 import GamesListContent from "views/content/GamesListContent";
 
 const AllGamesContainer = () => {
-  const { data, loading } = useSelector((state) => state.mainContent);
+  const { data, firstLoading, loading } = useSelector(
+    (state) => state.mainContent
+  );
   const dispatch = useDispatch();
   const { search } = useLocation();
 
@@ -16,12 +21,23 @@ const AllGamesContainer = () => {
     dispatch(fetchContent(ROUTE_GAMES + search));
   }, [dispatch, search]);
 
+  const getMoreData = () => {
+    if (data?.nextPageQuery) {
+      dispatch(fetchMoreData(`${ROUTE_GAMES}-more?${data.nextPageQuery}`));
+    }
+  };
+
   return (
     <>
-      {loading || !data.games ? (
-        <GamesListContent isLoading />
+      {firstLoading || !data.games ? (
+        <GamesListContent firstLoading />
       ) : (
-        <GamesListContent isLoading={false} data={data.games} />
+        <GamesListContent
+          firstLoading={false}
+          loading={loading}
+          data={data}
+          getMoreData={getMoreData}
+        />
       )}
     </>
   );
